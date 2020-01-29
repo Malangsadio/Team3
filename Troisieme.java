2d385bd769fcb15b0536b0b1170e25b52d63fb47
Malangsadio <58328693+Malangsadio@users.noreply.github.com>
2020-01-29 08:53:10 +0100

    Add files via upload

---
+++ Troisieme.java
@@ -0,0 +1,14 @@
+package com.example.geschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Troisieme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_troisieme);
+    }
+}
